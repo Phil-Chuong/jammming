8c6112d2f5f0056919d38deb00f27bf56eeccc71
Phil-Chuong <68432036+Phil-Chuong@users.noreply.github.com>
2023-08-28 11:50:59 +0100

    save playlist resolved

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
 
     this.state = {
       searchResults: [],
-        playlistName: 'My PlayList',
+        playlistName: 'My Playlist',
         playlistTracks: []
     };
 
